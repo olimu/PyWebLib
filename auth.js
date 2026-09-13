4774c93751b79595b039999729abe78e8c2c3f3b
Olina Mukherjee <69422108+olimu@users.noreply.github.com>
2026-09-12 22:55:27 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -92,8 +92,8 @@
                '<span class="pwl-acct-name">' + esc(displayName()) + '</span>' +
              '</button>' +
              '<div class="pwl-acct-menu" role="menu">' +
-               '<a class="pwl-acct-item" href="/community/?mine=1" role="menuitem">My programs</a>' +
-               '<a class="pwl-acct-item" href="/settings/" role="menuitem">Settings</a>' +
+               '<a class="pwl-acct-item" href="community/?mine=1" role="menuitem">My programs</a>' +
+               '<a class="pwl-acct-item" href="settings/" role="menuitem">Settings</a>' +
                rename +
                '<button type="button" class="pwl-acct-item" data-pwl="signout" role="menuitem">Sign out</button>' +
              '</div>' +
